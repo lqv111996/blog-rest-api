9b32b768d61bbf5cb9d57f7f34ac89144d8222b5
lqv111996 <37047063+lqv111996@users.noreply.github.com>
2023-06-21 23:24:35 +0700

    add post controller

---
+++ src/main/java/com/project/blog/rest/api/service/PostService.java
@@ -0,0 +1,20 @@
+package com.project.blog.rest.api.service;
+
+import com.project.blog.rest.api.payload.PostDto;
+import com.project.blog.rest.api.payload.PostResponse;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+
+    List<PostDto> getPostsByCategory(Long categoryId);
+}
